def1fa9f89dbcdb3c71ca8861829171de3fa4962
Rahul Gupta <rahuldevit2000@gmail.com>
2025-12-31 19:30:17 +0530

    Update ProductRepository.java
    Updated Files

--- src/main/java/com/cglia/springcrud/repository/ProductRepository.java
+++ src/main/java/com/cglia/springcrud/repository/ProductRepository.java
@@ -1,13 +1,10 @@
 package com.cglia.springcrud.repository;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
-
 import org.springframework.stereotype.Repository;
-
 import com.cglia.springcrud.bean.ProductBean;
 
 @Repository
 public interface ProductRepository extends JpaRepository<ProductBean, Integer> {
 
 }
+
