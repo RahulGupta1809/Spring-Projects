9321cae6cf80410431f02ad56caa207ed70fe73a
Rahul Gupta <rahuldevit2000@gmail.com>
2026-02-22 15:04:18 +0530

    Update ConsumeWebService.java
    Removing Nonusing Space

--- src/main/java/com/cglia/springcrud/controller/ConsumeWebService.java
+++ src/main/java/com/cglia/springcrud/controller/ConsumeWebService.java
@@ -1,7 +1,5 @@
 package com.cglia.springcrud.controller;
-
 import java.util.Arrays;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -58,3 +56,4 @@ public class ConsumeWebService {
         }
     }
 }
+
