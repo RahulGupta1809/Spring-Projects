2f57270b2d6af900c1e967d3071d44b19d7ed8d7
Rahul Gupta <rahuldevit2000@gmail.com>
2026-03-17 21:44:32 +0530

    Update EmployeeController.java
    New

--- src/main/java/com/cglia/springcrud/bean/ProductBean.java
+++ src/main/java/com/cglia/springcrud/bean/ProductBean.java
@@ -5,7 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-
+//Bean Classes NEw
 @Entity
 @Table(name = "product")
 public class ProductBean {
