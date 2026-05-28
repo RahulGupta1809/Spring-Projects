3e96a1cac9b0f182a4d7e5fe76c7c68da0276974
Rahul Gupta <rahuldevit2000@gmail.com>
2026-05-28 06:45:05 +0530

    Clean up comments in SpringcrudApplicationTestsTest
    Removed unnecessary comment lines in test file.

--- src/test/java/com/cglia/springcrud/SpringcrudApplicationTests.java
+++ src/test/java/com/cglia/springcrud/SpringcrudApplicationTests.java
@@ -2,7 +2,7 @@ package com.cglia.springcrud;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+//Test File
 @SpringBootTest
 class SpringcrudApplicationTests {
 //Test File 
