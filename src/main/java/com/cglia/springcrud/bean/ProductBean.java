160aaacfc1a8f6c6949aeb554fb45ba8e45c9e99
Rahul Gupta <rahuldevit2000@gmail.com>
2026-05-28 06:46:17 +0530

    Changes
    NEw

--- src/main/java/com/cglia/springcrud/bean/ProductBean.java
+++ src/main/java/com/cglia/springcrud/bean/ProductBean.java
@@ -5,7 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-//Bean Classes New to
+//Bean Classes New to BEan
 @Entity
 @Table(name = "product")
 public class ProductBean {
